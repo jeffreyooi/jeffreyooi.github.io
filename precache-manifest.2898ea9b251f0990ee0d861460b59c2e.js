f0ed3155fad4ca29332c23efd25d1dab113ace54
Jeffrey Ooi <jeffrey950310@gmail.com>
2020-04-26 22:13:03 +0800

    Updates

---
+++ precache-manifest.2898ea9b251f0990ee0d861460b59c2e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951b58da3662b7931ce69cbfc332415d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae1f71f0f0a9e3528c71",
+    "url": "/static/css/main.884d7e8a.chunk.css"
+  },
+  {
+    "revision": "4d7b424cc82daf56fe66",
+    "url": "/static/js/2.880b548f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.880b548f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1f71f0f0a9e3528c71",
+    "url": "/static/js/main.226b579c.chunk.js"
+  },
+  {
+    "revision": "0f08bcb18274dcbd8675",
+    "url": "/static/js/runtime-main.99f1d94e.js"
+  },
+  {
+    "revision": "addbb673ce35a6f0a1e08b2757a93846",
+    "url": "/static/media/FiraCode.addbb673.ttf"
+  },
+  {
+    "revision": "080cf80916fe5c059425e8a4c0674cd2",
+    "url": "/static/media/email.080cf809.svg"
+  },
+  {
+    "revision": "96f0c2a4e14ac5fe4d93a00df13f6e7b",
+    "url": "/static/media/image.96f0c2a4.jpg"
+  },
+  {
+    "revision": "54b8ef4b9587b97028987516b987a5f0",
+    "url": "/static/media/instagram.54b8ef4b.svg"
+  },
+  {
+    "revision": "0621d6d4fadc9171772e20527629d7a6",
+    "url": "/static/media/linkedin.0621d6d4.svg"
+  }
+]);
